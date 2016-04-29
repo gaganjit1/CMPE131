a13fcd13f68815a947ca284f5aa26cdfd9cd0662
aman212006 <andrewahwal@gmail.com>
2016-04-29 12:31:17 -0700

    Added files via upload

---
+++ 4-29-16 Survey Code/www/index.php
@@ -0,0 +1,36 @@
+<?php
+require 'class.phpmailer.php';
+echo '<script>
+
+function validateForm() {
+    var x = document.forms["studentid"]["sjsuid"].value;
+    if (( x.length != 9 ) || (x.match(/[a-z]/i)) ) {
+        alert("SJSU ID not found. Please use your SJSU 9 digit ID.");
+        return false;
+    }
+}
+
+function validateEmail() {
+	alert("Invalid email.");
+    return false;
+}
+
+
+
+</script>'; 
+
+include('/menu.php');
+
+//student login form
+echo '<div class="container"><br>';
+echo '<h2 class="white">The Smart Major</h2>
+<h3 class="pink">A smart  way to start your future</h3>';
+
+echo '<br>';
+echo '<span class="white">SJSU Login: </span>';
+echo '<form name="studentid" id="studentid" method="get" action="survey.php" onsubmit="return validateForm()">
+<input type="text" name="sjsuid"></input>
+<input type="submit" value="Submit">
+</form></div>';
+
+?>
---
+++ 4-29-16 Survey Code/www/python/Vector.py
@@ -0,0 +1,31 @@
+import math
+import SQL
+
+def vectordist(a, b):
+    c = []
+    d = i = 0
+    while i < len(a):
+        x = a[i] - b[i]
+        c.append(x)
+        c[i] = c[i] * c[i]
+        d = d + c[i]
+        i += 1
+    d = math.sqrt(d)
+
+    return d
+
+def distcheck(a):
+    avg = SQL.getavg()
+    cmpe = vectordist(a, avg[0])
+    cheme = vectordist(a, avg[1])
+    ee = vectordist(a, avg[2])
+    meche = vectordist(a, avg[3])	
+    minm = min(cmpe, cheme, ee, meche)
+    if  minm == cmpe:
+        return "Computer Engineering", "CmpE"
+    elif minm == cheme:
+        return "Chemical Engineering", "ChemE"
+    elif minm == ee:
+        return "Electrical Engineering", "EE"
+
+    return "Mechanical Engineering", "MechE"
