e73a6242c9305d1f94a831598bc7f456978b5fb9
gaganjit1 <gaganjit2@gmail.com>
2016-05-09 20:27:13 -0700

    Final Code with Various Fixes.

---
+++ Final Code/admin.php
@@ -0,0 +1,48 @@
+<?php
+include('/menu.php');
+
+$password="";
+
+echo '<script>
+
+function validate() {
+    var x = document.forms["admin"]["password"].value;
+    if (x != "admin") {
+        alert("Wrong password. Try again!");
+        return false;
+    }
+}
+
+</script>'; 
+
+if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+	$password = $_POST['password'];
+}
+
+if($password == "admin")
+{
+/* todo set admin login cookie so we dont need password everytime
+$user = array(
+    'id' => "admin",
+    'login' => $login
+)
+
+-- login
+setcookie("loginCredentials", $user, time() * 7200); 
+
+-- logout
+setcookie("loginCredentials", "", time() - 3600); 
+*/
+        include("secure.php");
+}
+else
+{
+//admin login form
+echo '<div class="container"><br>';
+echo '<span class="white">Admin password: </span>';
+echo '<form name="admin" id="admin" method="post" action="admin.php" onsubmit="return validate()">
+<input type="password" name="password"></input>
+<input type="submit" value="Submit">
+</form></div>';
+	
+}
